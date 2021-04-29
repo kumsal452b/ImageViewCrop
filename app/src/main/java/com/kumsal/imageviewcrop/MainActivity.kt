48095dc5fd031c2f075a384e7d87b287e74ee779
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-04-30 02:55:39 +0300

    add some resorurces element

--- app/src/main/java/com/kumsal/imageviewcrop/MainActivity.kt
+++ app/src/main/java/com/kumsal/imageviewcrop/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.kumsal.imageviewcrop
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import java.io.File
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
+    lateinit var imageView:ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageView=findViewById(R.id.imageView)
+        var file=File(URI.create())
+        var bitmap=BitmapFactory.decodeFile()
+
+
     }
 }
