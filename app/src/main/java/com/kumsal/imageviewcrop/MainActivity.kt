a5f6cadafc9d620a53d71957db02ca7d1fd96c56
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-05-01 01:06:39 +0300

    test is not succesfuly

--- app/src/main/java/com/kumsal/imageviewcrop/MainActivity.kt
+++ app/src/main/java/com/kumsal/imageviewcrop/MainActivity.kt
@@ -3,13 +3,12 @@ package com.kumsal.imageviewcrop
 import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.TypedValue
 import android.widget.ImageView
-import java.io.File
-import java.io.InputStream
-import java.net.URI
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var imageView:ImageView
@@ -18,17 +17,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         imageView=findViewById(R.id.imageView)
 
-        var bitmap=BitmapFactory.decodeResource(resources,R.drawable.test)
+        var bitmap=BitmapFactory.decodeResource(resources, R.drawable.woman)
         var scaling=Scalling()
-        val dip = 430f
+//        val displayMetrics = DisplayMetrics()
+//        windowManager.defaultDisplay.getMetrics(displayMetrics)
+//        val height = displayMetrics.heightPixels
+//        val width = displayMetrics.widthPixels
+        val dip = 500f
         val r: Resources = Resources.getSystem()
         val px = TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP,
                 dip,
                 r.displayMetrics
         )
-        imageView.setImageBitmap(Bitmap.createBitmap(bitmap,0,0,
-                px.toInt(),px.toInt()))
+        imageView.setImageBitmap(scaling.scaleToFitHeight(bitmap,px.toInt()))
 
+//        imageView.setImageBitmap(bitmap)
     }
 }
